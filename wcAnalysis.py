b29e265d0872124aa728e8445f95532145282ea9
harrisonzhu508 <harrisonzhu5080@gmail.com>
2018-12-19 13:39:34 +0000

    fix code

--- wcAnalysis.py
+++ wcAnalysis.py
@@ -1,8 +1,17 @@
-#The aim of this project is to predict the scores head-to-head matches of world cup 2018 in Russia.
-#In particular, we work under the Bayesian framework, using the Poisson regression likelihood with a normalised Gaussian prior. Our 3
-#approximations will be the Laplace approximation, Metropolis-Hastings and Gaussian Variational Approximation (GVA). Our prediction is
-#made using sampling. In particular, using the large law of numbers and the ergodic central limit theorem.
-#Our dataset contains head-to-head match scores between the 32 teams from 1995 to 2018. We distinguish between home and away teams. 
+"""
+The aim of this project is to predict the scores head-to-head matches of world cup 2018 in Russia.
+
+In particular, we work under the Bayesian framework, using the Poisson regression likelihood with a normalised Gaussian prior. Our 3
+approximations will be the:
+
+- Laplace approximation, 
+- Metropolis-Hastings and 
+- Gaussian Variational Approximation (GVA). 
+
+Our prediction is made using sampling. In particular, using Monte Carlo integration.
+
+Our dataset contains head-to-head match scores between the 32 teams from 1995 to 2018. We distinguish between home and away teams. 
+"""
 
 #import libraries
 import pandas as pd
@@ -12,6 +21,7 @@ import math
 from math import e, pi, log, factorial
 import scipy
 from scipy.stats import poisson
+
 #import pytorch libraries
 import torch
 from torch import Tensor
@@ -28,10 +38,22 @@ for i in range(len(Teams_main)):
     Teams[Teams_main.Teams[i]] = i
 
 #----------------------------------------------------------------------------------------------------------------------------------
+#Define the un_Posterior and its derivatives
 #Here we develop the functions we will use for our analysis
-#Define the Posterior and its derivatives
-#likelihood function
 def likelihood(theta, train_input):
+    """
+    likelihood function
+
+    Input:
+
+        theta: model parameters
+        train_input: training set
+
+    Output:
+
+        prod: the likelihood
+
+    """
     prod = 1
     #collect coefficients
     Delta = theta[0]
@@ -54,50 +76,96 @@ def likelihood(theta, train_input):
 
     return prod
 
-def posterior(theta, train_input):
+def un_posterior(theta, train_input):
+    """
+    Define the unnormalised posterior distribution
+
+    input:
+
+        theta: model parameters
+        train_input: training set
+
+    output:
+
+        unPosterior: unnormalised posterior
+    """
     prior = e ** (-0.5 * theta.norm() ** 2)
+    unPosterior = likelihood(theta, train_input) * prior
 
-    return likelihood(theta, train_input) * prior
+    return unPosterior
 
 
 
-#Simple log unnormalised-posterior we call log_MAP
-#train_input is our dataframe
 def log_MAP(theta, train_input):
+    """
+    Simple log unnormalised-posterior we call log_MAP
+
+    input:
+
+        theta: model parameters
+        train_input: training set
+
+    output:
+
+        somme2: log unnormalised-posterior
+    """
     #collect coefficients
     #Delta = theta[0]
     #alphas = theta[1:21]
     #betas = theta[21:41]
+    
     #calculate regularisor
     regularisor = -0.5 * theta.norm() ** 2
+    
     #second sum
     somme2 = regularisor
     for i in range(len(train_input)):
         indexH = Teams[train_input.localTeam[i]]
         indexA = Teams[train_input.visitorTeam[i]]
-        L = -( e ** (theta[0] + theta[1+indexH] - theta[32+indexA]) + e ** (theta[0] + theta[1+indexA] - theta[32+indexH]) ) + float(train_input.localGoals[i]) * (theta[1+indexH] - theta[32+indexA]) + float(train_input.visitorGoals[i]) * (theta[1+indexA] - theta[32+indexH])
+
+        L = -( e ** (theta[0] + theta[1+indexH] - theta[32+indexA]) \
+            + e ** (theta[0] + theta[1+indexA] - theta[32+indexH]) ) \
+            + float(train_input.localGoals[i]) * (theta[1+indexH] - theta[32+indexA]) \
+            + float(train_input.visitorGoals[i]) * (theta[1+indexA] - theta[32+indexH])
+
         somme2 = somme2 + L
 
-    return(somme2)
+    return somme2
 
 def gradient_log_MAP(theta, train_input):
+    """
+    Gradient of the log unnormalised-posterior
+
+    input:
+
+        theta: model parameters
+        train_input: training set
+
+    output:
+
+        gradient: gradient of the log-posterior
+    """
     #collect coefficients
     Delta = theta[0]
     alphas = theta[1:32]
     betas = theta[32:63]
+    
     #grad vector
     gradient = torch.zeros(63,1)   
+    
     #first component with respect to Delta
     somme1 = -Delta
     for i in range(len(train_input)):
         indexH = Teams[train_input.localTeam[i]]
         indexA = Teams[train_input.visitorTeam[i]]
-        somme1 = somme1 - e ** (Delta + alphas[indexH] - betas[indexA]) - e ** (alphas[indexA] - betas[indexH]) + float(train_input.visitorGoals[i])
+        somme1 = somme1 - e ** (Delta + alphas[indexH] - betas[indexA]) \
+                - e ** (alphas[indexA] - betas[indexH]) + float(train_input.visitorGoals[i])
     gradient[0] = float(somme1)
 
     #components
     somme2 = 0
     somme3 = 0
+
     for k in range(31):
         somme2 = -alphas[k]
         somme3 = -betas[k]
@@ -118,18 +186,34 @@ def gradient_log_MAP(theta, train_input):
 
 
 def Hess_log_MAP(theta, train_input):
+    """
+    The hessian of the log posterior
+    
+    input:
+
+        theta: model parameters
+        train_input: training set
+
+    output:
+
+        Tensor(Hess): Hessian of the log posterior
+    """
     #collect coefficients
     Delta = theta[0]
     alphas = theta[1:32]
     betas = theta[32:63]
+
     #grad vector
     Hess = np.zeros([63, 63])
+    
     #first component with respect to Delta
     somme1 = -1
+    
     for k in range(len(train_input)):
         indexH = Teams[train_input.localTeam[k]]
         indexA = Teams[train_input.visitorTeam[k]]
         somme1 = somme1 - e ** (Delta + alphas[indexH] - betas[indexA]) - e ** (alphas[indexA] - betas[indexH])
+    
     Hess[0][0] = float(somme1)
 
     #2nd components with respect to delta alphai and alphai^2 and betai^2
@@ -137,6 +221,7 @@ def Hess_log_MAP(theta, train_input):
     somme3 = 0
     somme4 = -1
     somme5 = -1
+    
     for k in range(31):
         somme2 = 0
         somme3 = 0
@@ -168,6 +253,7 @@ def Hess_log_MAP(theta, train_input):
 
     #6th components with respect to alphai betai
     somme6 = 0
+    
     for k in range(31):
         for l in range(31):
             somme6 = 0
@@ -188,18 +274,34 @@ def Hess_log_MAP(theta, train_input):
 
 
 #optimisation tools
-#we use the minibatch stochastic gradient descent
 
-def SGD_batch(train, log_MAP, gradient_log_MAP, theta, lr = 1e-1, nb_epochs = 250):
+def SGD_batch(train_input, log_MAP, gradient_log_MAP, theta, lr = 1e-1, nb_epochs = 250):
+    """
+    We use the minibatch stochastic gradient descent
+
+    input:
+
+        train_input:  training set
+        log_MAP:  the log-posterior function
+        gradient_log_MAP: the gradient of the log-posterior function
+        theta:  parameters of our model
+        lr = 1e-1:  learning rate (descent rate)
+        nb_epochs = 250:    number of training epochs 
+
+    output:
+
+        theta:  maximum a posteriori (MAP) estimator
+        record_loss: the training loss
+    """
     #initialisation
-    train_input = train
+    train_input
     #load up the criterion and gradient function
     criterion = log_MAP
     gradient = gradient_log_MAP
 
     #number of epochs for training
     #nb_epochs = 250
-    n = len(train) #number of samples
+    n = len(train_input) #number of samples
     mini_batch_size = n // 20
     sum_loss = 0
     count_of_decrease = 0
@@ -207,6 +309,7 @@ def SGD_batch(train, log_MAP, gradient_log_MAP, theta, lr = 1e-1, nb_epochs = 25
 
     #gradient descent
     for e in range(0, nb_epochs):
+
         sum_loss = 0
         for b in range(0, n, mini_batch_size):
             theta = theta + lr * gradient(theta, train_input[b:b + mini_batch_size].reset_index())
@@ -226,10 +329,24 @@ def SGD_batch(train, log_MAP, gradient_log_MAP, theta, lr = 1e-1, nb_epochs = 25
 
     return theta, record_loss
 
-#Markov chain sampling approach
+#Markov chain Monte Carlo sampling approach
 
-#define the acceptance ratio
-def posterior_ratio(theta_proposal, theta_start, train_input):
+def un_posterior_ratio(theta_proposal, theta_start, train_input):
+    """
+    Define the acceptance ratio
+
+    input:
+        
+        theta_proposal: value obtained by proposal distribution
+        theta_start: the current value of the Markov chain
+        train_input: training set
+
+    output:
+
+        acceptance ratio
+
+
+    """
 
     #collect coefficients
     Deltaprop = theta_proposal[0]
@@ -266,14 +383,43 @@ def posterior_ratio(theta_proposal, theta_start, train_input):
     return prod1 * prod
 
 #define the log of the acceptance ratio
-def posterior_ratio_log(theta_proposal, theta_start, train_input):
-    
+def un_posterior_ratio_log(theta_proposal, theta_start, train_input):
+    """
+    Define the log of the acceptance ratio and then calculate the acceptance ratio
+
+    input:
+        
+        theta_proposal: value obtained by proposal distribution
+        theta_start: the current value of the Markov chain
+        train_input: training set
+
+    output:
+
+        acceptance ratio
+    """
+
+
     val = log_MAP(theta_proposal, train_input) - log_MAP(theta_start, train_input)
     
     return e ** val
     
-#the metroplis-hastings algorithm function
 def MH_RW_tuned(n, theta0, train_input, lamda, batch_size):
+    """
+    The random walk Metroplis-Hastings algorithm with uniform proposal
+
+    input:
+    
+    n: number of samples
+    theta0: initial value in the Markov chain
+    train_input: training set
+    lamda: factor of the size of the walk
+    batch_size: batch size to select a lamda such that it gives a good acceptance probability
+
+    output:
+
+        Samples_storage: samples of a Markov chain
+    """
+    
     #for this algorithm, we should start from a relatively small, negative value of lambda
     theta0 = theta0.view(-1)
     #d = dimension of theta
@@ -292,7 +438,7 @@ def MH_RW_tuned(n, theta0, train_input, lamda, batch_size):
         for i in range(batch_size):
             theta_proposal = theta_start + Tensor(np.random.uniform(-1,1,d)*lamda_final)
             #rejection/acceptance
-            ratio = posterior_ratio_log(theta_proposal, theta_start, train_input)
+            ratio = un_posterior_ratio_log(theta_proposal, theta_start, train_input)
             p = min(1, ratio)
             #rejection step
             bern = np.random.binomial(1,p,1)
@@ -319,7 +465,7 @@ def MH_RW_tuned(n, theta0, train_input, lamda, batch_size):
     for i in range(n):
         theta_proposal = theta_start + Tensor(np.random.uniform(-1,1,d)*lamda_final)
         #rejection/acceptance
-        ratio = posterior_ratio_log(theta_proposal, theta_start, train_input)
+        ratio = un_posterior_ratio_log(theta_proposal, theta_start, train_input)
         p = min(1,ratio)
 
         #rejection step
@@ -333,9 +479,22 @@ def MH_RW_tuned(n, theta0, train_input, lamda, batch_size):
     return Chain, np.mean(Ratio), C/n
 
 #Prediction tools
-
-#Laplace_sample samples from what we obtain from the laplace approximation method
 def Laplace_sample(mu, covariance, nb_sample1, nb_sample2):
+    """
+    Laplace_sample samples from what we obtain from the laplace approximation method and the likelihood.
+    Then we use these to perform prediction via Monte Carlo integration
+
+    input:
+        
+        mu: estimated mean of the Gaussian from the Laplace approximation
+        covariance: estimated covariance matrix of the Gaussian from the Laplace approximation
+        nb_sample1: number of samples from the posterior
+        nb_sample2: number of samples from the likelihood
+
+    output:
+
+        Samples_storage: stores the sample from the posterior and likelihood (dependent on the sample from posterior)
+    """
 
     #step 1: generate nb_sample samples of theta
     theta_samples = Tensor(np.random.multivariate_normal(mu.view(-1), covariance, nb_sample1))
@@ -370,8 +529,21 @@ def Laplace_sample(mu, covariance, nb_sample1, nb_sample2):
     Samples_storage = Samples_storage / (nb_sample1 * nb_sample2)
     return Samples_storage
 
-#similar to the laplace sampler, we skip the step where we sample thetas from the posterior
 def chain_predictor(theta_samples, nb_sample1, nb_sample2):
+    """
+    Similar to the laplace sampler, we skip the step where we sample thetas from the unnormalised posterior
+    Then we use these to perform prediction via Monte Carlo integration
+
+    input:
+        
+        theta_samples: samples obtained from RW Metropolis-Hastings
+        nb_sample1: number of samples from the posterior
+        nb_sample2: number of samples from the likelihood
+
+    output:
+
+        Samples_storage: stores the sample from the posterior and likelihood (dependent on the sample from posterior)
+    """
 
     #step 1: for each of the 380 needed predictions, generate 100 samples from each of the 1000
     #samples of theta
@@ -383,6 +555,7 @@ def chain_predictor(theta_samples, nb_sample1, nb_sample2):
         for j in range(20):
             if j != i:
                 for k in range(nb_sample1):
+                    
                     #parameters
                     thetak = theta_samples[k]
                     delta = thetak[0]
@@ -394,8 +567,7 @@ def chain_predictor(theta_samples, nb_sample1, nb_sample2):
                     mu_ij_away = e ** (alphaj - betai)
 
                     team1 = sum(np.random.poisson(mu_ij_home, nb_sample2))
-                    team2 = sum(np.
-                                random.poisson(mu_ij_away, nb_sample2))
+                    team2 = sum(np.random.poisson(mu_ij_away, nb_sample2))
 
                     Samples_storage[i][j] = Samples_storage[i][j] + Tensor([float(team1), float(team2)])
 
@@ -404,11 +576,23 @@ def chain_predictor(theta_samples, nb_sample1, nb_sample2):
     return Samples_storage
 
 #Gaussian Variational Approximation (GVA)
-
-#define the Evidence Lower Bound (ELBO)
 def ELBO(data, mu_n, L_n):
-    #data is a tensor storing the thetas
-    #mu_n, L_n initialised at some points
+    """
+    Define the Evidence Lower Bound (ELBO).
+
+    input:
+       
+        data: is a tensor storing the thetas
+        mu_n: initialised at some points
+        L_n: initialised at some points
+
+    output:
+
+        somme: the  ELBO
+
+
+    """
+    
     #let's say the variance covariance matrix is the identity
     l = 100
     d = len(mu_n)
@@ -421,9 +605,23 @@ def ELBO(data, mu_n, L_n):
         somme = somme + log_MAP(Eeta + mu_n, data)
     somme = -somme/l
     
-    return(somme)
+    return somme
 
 def gradmu_ELBO(data, mu_n, L_n):
+    """
+    Compute the gradient of the ELBO with respect to mu
+
+    input:
+
+        data: is a tensor storing the thetas
+        mu_n: initialised at some points
+        L_n: initialised at some points
+
+    output:
+
+        somme: gradient of the ELBO
+
+    """
     l = 100
     d = len(mu_n)
     e_L = Tensor(scipy.linalg.expm(L_n.numpy()))
@@ -435,11 +633,22 @@ def gradmu_ELBO(data, mu_n, L_n):
         somme = somme + gradient_log_MAP(Eeta + mu_n, data)
     somme = -somme/l
     
-    
-    return(somme)
+    return somme
 
-#the gradient of the ELBO using the Hessian approach
 def gradL_ELBO1(data, mu_n, L_n):
+    """
+    The gradient of the ELBO with respect to the matrix L using the Hessian approach
+
+    input:
+        
+        data: is a tensor storing the thetas
+        mu_n: initialised at some points
+        L_n: initialised at some points
+
+    output:
+
+        somme: gradient of the ELBO with respect to the matrix L
+    """
     
     l = 100
     d = len(mu_n)
@@ -455,10 +664,23 @@ def gradL_ELBO1(data, mu_n, L_n):
     somme = -0.5 * (somme + somme.t())
     somme = somme + torch.eye(d)
     
-    return(somme)
+    return somme
 
-#the gradient of the ELBO using the gradient approach
 def gradL_ELBO2(data, mu_n, L_n):
+    """
+    The gradient of the ELBO using the gradient approach 
+    
+    input:
+        
+        data: is a tensor storing the thetas
+        mu_n: initialised at some points
+        L_n: initialised at some points
+
+    output:
+
+        somme: gradient of the ELBO with respect to the matrix L
+
+    """
     
     l = 100
     d = len(mu_n)
@@ -473,10 +695,23 @@ def gradL_ELBO2(data, mu_n, L_n):
     somme = -0.5 * (somme + somme.t())
     somme = somme + torch.eye(d)
     
-    return(somme)
+    return somme
 
-#The GVA via the gradient ELBO gradient
 def GVA_grad(initial_vals, data, lr, plot_yes = True):
+    """
+    The GVA via the gradient ELBO gradient
+
+    initial_vals: initial values of mu_n and L_n
+    data: is a tensor storing the thetas
+    lr: the learning rate or descent rate
+    plot_yes: Default = True. Plots the ELBO
+
+    output:
+
+        The computed mu_n and L_n
+
+    """
+    
     #initialisation
     #d = dimension of mu
     #l = length of data
@@ -512,8 +747,21 @@ def GVA_grad(initial_vals, data, lr, plot_yes = True):
 
     return mu_n, L_n
 
-#predict head-to-head score function
 def results(hometeam, awayteam, scores_mat):
+    """
+    Predict head-to-head score function
+
+    input:
+
+        hometeam: name of the home team as string
+        awayteam: name of the away team as string
+        scores_mat: stores the scores
+
+    output:
+
+        The result of the game
+    """
+
     #hometeam, awayteam as strings
     indexh = 2*Teams[hometeam]
     indexa = Teams[awayteam]
